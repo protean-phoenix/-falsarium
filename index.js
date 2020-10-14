ed915bfbf74f43aa2e40f72b05bb51d081072ccd
Claudius King <tezcatlipoca7123@gmail.com>
2020-10-13 21:38:20 -0700

    Add playfield

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 window.onload = function(){
-  helloWorldElement = document.createElement("p");
-  helloWorldElement.innerHTML = "hello world!";
-  document.getElementsByTagName("Body")[0].append(helloWorldElement);
+  console.log("width: "+window.innerWidth);
+  console.log("height: "+window.innerHeight);
+  const two = new Two({width:900, height:900}).appendTo(document.getElementsByTagName('Body')[0]);
+  rect = two.makeRectangle(450, 450, 850, 850);
+  rect.fill = '#AAAAAA';
+  rect.stroke = '#555555';
+  rect.linewidth = 7;
+  two.update();
 };
