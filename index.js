249b2ed6b579ef80b31dd5e9ec3751d7abe097db
Claudius King <tezcatlipoca7123@gmail.com>
2020-10-14 00:38:45 -0700

    Add player movement

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ window.onload = function(){
   const playfield = two.makeRectangle(450, 450, 850, 850);
   const player = two.makeRectangle(450, 450, 50, 50);
 
+  const upVector = new Two.Vector(0, -5);
+  const downVector = new Two.Vector(0, 5);
+  const leftVector = new Two.Vector(-5, 0);
+  const rightVector = new Two.Vector(5, 0);
+
   let animals = ['wolf', 'gecko', 'finch', 'trout', 'beaver', 'rhino', 'mantis', 'hornet', 'skink', 'salamander', 'vulture', 'crow', 'spider', 'platypus'];
   let colors = [{name:'red', fill:'#FF5555', stroke:'#DD3333'},
     {name:'orange', fill:'#FFAA55', stroke:'#DD8833'},
@@ -16,7 +21,7 @@ window.onload = function(){
   let animal = animals.splice(Math.floor(Math.random()*animals.length), 1)[0];
 
   const nameText = new Two.Text('you are '+color.name+' '+animal+'.', 450, 100,
-    {fill:color.fill, stroke:color.stroke, size:25});
+    {fill:color.fill, stroke:color.stroke, size:50});
   two.add(nameText);
 
   playfield.fill = '#000000';
@@ -26,6 +31,22 @@ window.onload = function(){
   player.fill = color.fill;
   player.stroke = color.stroke;
   player.linewidth = 7;
+
+  document.addEventListener('keydown', function(event){
+    console.log(event.key);
+    if(event.key == 'ArrowDown' && player.translation.y < 843){
+      player.translation.addSelf(downVector);
+    }
+    if(event.key == 'ArrowUp' && player.translation.y > 57){
+      player.translation.addSelf(upVector);
+    }
+    if(event.key == 'ArrowLeft' && player.translation.x > 57){
+      player.translation.addSelf(leftVector);
+    }
+    if(event.key == 'ArrowRight' && player.translation.x < 843){
+      player.translation.addSelf(rightVector);
+    }
+  });
 
   two.bind('update', function(frameCount){
     if(frameCount > 5*60 && nameText.opacity > 0){
