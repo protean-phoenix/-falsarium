a983dd7eaa4332fa1ad069ba1bd2ecd526c015e3
Claudius King <tezcatlipoca7123@gmail.com>
2020-10-14 05:14:11 -0700

    Add opponent square

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ window.onload = function(){
   const rightBoundry = playfieldWidth - 32;
   const topBoundry = 40;
   const bottomBoundry = playfieldHeight - 32;
+
   const two = new Two({width:twoWidth, height:twoHeight}).appendTo(document.getElementById('userInterface'));
   const chat = document.createElement('div');
   const inputBox = document.createElement('input');
@@ -16,8 +17,14 @@ window.onload = function(){
   document.getElementById('userInterface').append(chat);
   document.getElementById('userInterface').append(inputBox);
 
+  let player = {};
+  let opponent = {};
+
   const playfield = two.makeRectangle(twoWidth/2, twoHeight/2, playfieldWidth, playfieldHeight);
-  const player = two.makeRectangle(twoWidth/2, twoHeight/2, 50, 50);
+  const playerSquare = two.makeRectangle(475, twoHeight/2, 50, 50);
+  const opponentSquare = two.makeRectangle(375, twoHeight/2, 50, 50);
+  player.square = playerSquare;
+  opponent.square = opponentSquare;
 
   const upVector = new Two.Vector(0, -5);
   const downVector = new Two.Vector(0, 5);
@@ -32,40 +39,53 @@ window.onload = function(){
     {name:'blue', fill:'#55AAFF', stroke:'#4488DD'},
     {name:'indigo', fill:'#5555FF', stroke:'#3333DD'},
     {name:'violet', fill:'#BBAAFF', stroke:'#9988DD'}];
-  let color = colors.splice(Math.floor(Math.random()*colors.length), 1)[0];
-  let animal = animals.splice(Math.floor(Math.random()*animals.length), 1)[0];
+  let playerColor = colors.splice(Math.floor(Math.random()*colors.length), 1)[0];
+  let playerAnimal = animals.splice(Math.floor(Math.random()*animals.length), 1)[0];
+  player.color = playerColor;
+  player.animal = playerAnimal;
+  let opponentColor = colors.splice(Math.floor(Math.random()*colors.length), 1)[0];
+  let opponentAnimal = animals.splice(Math.floor(Math.random()*animals.length), 1)[0];
+  opponent.color = opponentColor;
+  opponent.animal = opponentAnimal;
 
-  const fullName = color.name+' '+animal;
-  const nameText = new Two.Text('you are '+fullName+'.', 450, 100,
-    {fill:color.fill, stroke:color.stroke, size:50});
+  opponent.fullName = opponent.color.name+' '+opponent.animal;
+  player.fullName = player.color.name+' '+player.animal;
+  const nameText = new Two.Text('you are '+player.fullName+'.', 450, 100,
+    {fill:player.color.fill, stroke:player.color.stroke, size:50});
   two.add(nameText);
 
-  inputBox.style.color = color.fill;
+  inputBox.style.color = player.color.fill;
 
   playfield.fill = '#000000';
   playfield.stroke = '#555555';
   playfield.linewidth = 7;
 
-  player.fill = color.fill;
-  player.stroke = color.stroke;
-  player.linewidth = 7;
+  player.square.fill = player.color.fill;
+  player.square.stroke = player.color.stroke;
+  player.square.linewidth = 7;
+
+  opponent.square.fill = opponent.color.fill;
+  opponent.square.stroke = opponent.color.stroke;
+  opponent.square.linewidth = 7;
+
+  postToChat('glhf', opponent);
 
   document.addEventListener('keydown', function(event){
     console.log(event.key);
-    if(event.key == 'ArrowDown' && player.translation.y < bottomBoundry){
-      player.translation.addSelf(downVector);
+    if(event.key == 'ArrowDown' && player.square.translation.y < bottomBoundry){
+      player.square.translation.addSelf(downVector);
     }
-    if(event.key == 'ArrowUp' && player.translation.y > topBoundry){
-      player.translation.addSelf(upVector);
+    if(event.key == 'ArrowUp' && player.square.translation.y > topBoundry){
+      player.square.translation.addSelf(upVector);
     }
-    if(event.key == 'ArrowLeft' && player.translation.x > leftBoundry){
-      player.translation.addSelf(leftVector);
+    if(event.key == 'ArrowLeft' && player.square.translation.x > leftBoundry){
+      player.square.translation.addSelf(leftVector);
     }
-    if(event.key == 'ArrowRight' && player.translation.x < rightBoundry){
-      player.translation.addSelf(rightVector);
+    if(event.key == 'ArrowRight' && player.square.translation.x < rightBoundry){
+      player.square.translation.addSelf(rightVector);
     }
     if(event.key == 'Enter' && document.activeElement === inputBox){
-      postToChat(inputBox.value);
+      postToChat(inputBox.value, player);
       inputBox.value = '';
     }
   });
@@ -76,10 +96,10 @@ window.onload = function(){
     }
   }).play();
 
-  function postToChat(text){
+  function postToChat(text, character){
     const chatText = document.createElement('p');
-    chatText.innerHTML = fullName+': '+text;
-    chatText.style.color = color.fill;
+    chatText.innerHTML = character.fullName+': '+text;
+    chatText.style.color = character.color.fill;
 
     chat.prepend(chatText);
 
